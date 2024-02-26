3c0c9f587681b032407aff8a3f3575e5439bf458
Vlasta Vetra <vlastavetra@gmail.com>
2024-02-26 18:16:18 +0200

    linter fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,18 @@
 import { Suspense } from 'react';
 import './styles/index.scss';
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
-import { AppRouter } from "app/providers/router";
-import { LangSwitcher } from "shared/ui/LangSwitcher";
-
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
+    const { t } = useTranslation();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle Theme</button>
+            <button type="button" onClick={toggleTheme}>{t('Toggle Theme')}</button>
             <Suspense fallback="">
                 <LangSwitcher />
                 <AppRouter />
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,13 @@
 import React, { Suspense } from 'react';
-import { Route, Routes } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { useTranslation } from 'react-i18next';
 
 const AppRouter = () => {
+    const { t } = useTranslation();
+
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>{t('Loading')}</div>}>
             <Routes>
                 {Object.values(routeConfig).map(({ element, path }) => (
                     <Route
