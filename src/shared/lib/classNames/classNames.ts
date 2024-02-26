4e9f1c183669d91b16ae961832b0f591e66b3e37
Vlasta Vetra <vlastavetra@gmail.com>
2024-02-26 12:06:27 +0200

    added eslint, jest, hot replacement

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,16 +1,15 @@
-
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(
+    cls: string,
+    mods: Mods = {},
+    additional: string[] = [],
+): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
-            .map(([className]) => className)
-    ]
-        .join(' ');
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ].join(' ');
 }
-
-
-
